a4b78c1e3c74d1b7a50351a3cd8d6db253d2c8c7
itnug987 <srinivasgunti987@gmail.com>
2020-06-04 11:43:49 +0530

    Updates

---
+++ precache-manifest.889dc951922e9d31647ad8f2031adc9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c01c138b61f6c6b65d0c2744db16d65",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "04cf76ce0d3e333f566a",
+    "url": "/to-do-list/static/css/main.18427cfa.chunk.css"
+  },
+  {
+    "revision": "515ba02a1b394ac658e0",
+    "url": "/to-do-list/static/js/2.0fee2ff4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list/static/js/2.0fee2ff4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04cf76ce0d3e333f566a",
+    "url": "/to-do-list/static/js/main.ee869135.chunk.js"
+  },
+  {
+    "revision": "fa27aabefbb81c21a734",
+    "url": "/to-do-list/static/js/runtime-main.80f3eba2.js"
+  }
+]);
